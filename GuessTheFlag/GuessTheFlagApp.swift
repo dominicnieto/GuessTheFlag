8ae22ca92c5a04c34feeda34e62da24f5dd8e8e8
dominicnieto <dominicnieto27@gmail.com>
2024-08-20 19:09:30 -0600

    Initial Commit

---
+++ GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Dominic on 8/20/24.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
